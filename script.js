e76d8e0aff81f95c0af432cc08c6a5684b4d1758
Damon Haase <damon.haase@gmail.com>
2023-04-15 16:03:17 -0400

    Assign button variables to buttons on page.

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-firstNumber = 0;
-secondNumber = 0;
-operator = "";
-
+let firstNumber = 0;
+let secondNumber = 0;
+let operator = "";
+let displayValue = "";
+//aquire buttons
+const buttons = [];
+for (let i = 0; i < 16; i++) {
+    buttons[i] = document.querySelector('#btn' + i);
+}
+const buttonClear = buttons[10];
+const buttonMultiply = buttons[11];
+const buttonPlus = buttons[12];
+const buttonDecimal = buttons[13];
+const buttonEqual = buttons[14];
+const buttonMinus = buttons[15];
+//#region Basic operation functions
 function add(a, b) {
     return a + b;
 }
@@ -17,6 +29,7 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+//#endregion
 
 function operate(firstNumber, operator, secondNumber) {
     if (operator === "+") add(firstNumber, secondNumber);
